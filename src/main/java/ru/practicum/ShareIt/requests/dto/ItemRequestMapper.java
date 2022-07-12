b0c23b13fb6c8cd256055231eff8414132538db8
Sevko Lev <booogiman@yandex.ru>
2022-07-12 03:33:18 +0300

    Исправление Замечаний

--- src/main/java/ru/practicum/ShareIt/requests/dto/ItemRequestMapper.java
+++ src/main/java/ru/practicum/ShareIt/requests/dto/ItemRequestMapper.java
@@ -1,9 +1,14 @@
 package ru.practicum.shareit.requests.dto;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 import ru.practicum.shareit.requests.ItemRequest;
-
+//@Configuration
+@Component
 public class ItemRequestMapper {
-    public static ItemRequestDto toItemRequestDto(ItemRequest request) {
+
+    public ItemRequestDto toItemRequestDto(ItemRequest request) {
         return new ItemRequestDto(
                 request.getDescription(),
                 request.getRequestor(),
